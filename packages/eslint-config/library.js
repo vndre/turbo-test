df88250aae6b9dccc7af148949319d79a59aae46
vndre <dev@vndre.io>
2024-04-03 11:22:46 -0600

    init

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -1,8 +1,3 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
   plugins: ["only-warn"],
@@ -12,13 +7,6 @@ module.exports = {
   },
   env: {
     node: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
   },
   ignorePatterns: [
     // Ignore dotfiles
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,8 +1,3 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
     "eslint:recommended",
@@ -19,13 +14,6 @@ module.exports = {
     browser: true,
   },
   plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
   ignorePatterns: [
     // Ignore dotfiles
     ".*.js",
